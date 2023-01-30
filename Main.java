bb6cb53c86639d9682eed5780d166b0e834062e1
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2023-01-30 20:17:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 
-class Main {
+public class Main {
 	public static void main(String args[])
     {
 		System.out.print("Введите возводимое в степень: ");
