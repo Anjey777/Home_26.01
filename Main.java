3eae5c5d3b0110c24e55b18343a589e9f7945356
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2023-01-30 20:15:42 +0300

    Update and rename 1.java to Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Main {
+	public static void main(String args[])
+    {
+		System.out.print("Введите возводимое в степень: ");
+		Scanner scan1 = new Scanner(System.in);
+		int number = scan1.nextInt();
+        
+		System.out.print("Введите степень числа: ");
+		Scanner scan2 = new Scanner(System.in);
+		int power = scan2.nextInt();
+
+		System.out.println(Math.pow(number, power));
+        double result = Math.pow(number, power);
+        File file = new File ("output.txt");
+        try
+        {
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.println (result);
+            printWriter.close();       
+        }
+        catch (FileNotFoundException ex)  
+    {
+        System.out.println(result);
+    }
+    }
+}
